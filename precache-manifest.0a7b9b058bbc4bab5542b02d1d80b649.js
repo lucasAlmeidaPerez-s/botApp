e4fc987c676e03b04ec54f50cd79cf473334920b
lucas-ap <l.almeida.perez@gmail.com>
2019-07-01 00:23:04 -0300

    Updates

---
+++ precache-manifest.0a7b9b058bbc4bab5542b02d1d80b649.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5eee18b252de22cf3f9e7901df175e",
+    "url": "/botApp/index.html"
+  },
+  {
+    "revision": "88bd0a3edd1189f43336",
+    "url": "/botApp/static/css/main.062ac592.chunk.css"
+  },
+  {
+    "revision": "8544473094e1db3fc346",
+    "url": "/botApp/static/js/2.8191f02f.chunk.js"
+  },
+  {
+    "revision": "88bd0a3edd1189f43336",
+    "url": "/botApp/static/js/main.bbd179b8.chunk.js"
+  },
+  {
+    "revision": "e4560fb8b2b791526325",
+    "url": "/botApp/static/js/runtime~main.2727109a.js"
+  }
+]);
